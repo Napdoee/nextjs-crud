80e135727c59b96fc92cd8dd00099c55c76689b0
Napdoee <65323184+Napdoee@users.noreply.github.com>
2025-08-04 10:35:58 +0800

    Update page.tsx

--- app/contacts/create/page.tsx
+++ app/contacts/create/page.tsx
@@ -1,8 +1,10 @@
+import { BackButton } from "@/components/button";
 import CreateForm from "@/components/create-form";
 
 function CreateContactPage() {
   return (
     <div className="mx-auto mt-5 max-w-md">
+      <BackButton />
       <h1 className="mb-2 text-2xl text-center">Add New Contact</h1>
       <CreateForm />
     </div>
